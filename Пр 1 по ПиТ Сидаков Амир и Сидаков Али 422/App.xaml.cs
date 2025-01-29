6967c2e2ca6ffc2986a86ff7c974260e6acace25
Sidakov_Amir <121751618+Sidakov21@users.noreply.github.com>
2025-01-29 18:33:06 +0300

    Добавьте файлы проекта.

---
+++ Пр 1 по ПиТ Сидаков Амир и Сидаков Али 422/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Пр_1_по_ПиТ_Сидаков_Амир_и_Сидаков_Али_422
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
